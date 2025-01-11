cea7de61fa60db0bebaa80a8730ae4c42c432ce0
MRMUZAMIL50 <muzamilzakhmi687@gmail.com>
2025-01-12 00:29:30 +0500

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -64,7 +64,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/sigma.m4a' },
+            audio: { url:'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
